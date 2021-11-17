9869a5c33424fa1df0902fdc21dbc9b90234f61d
mbarquinv <marcbarquinv@gmail.com>
2021-11-17 10:47:50 +0100

    bro te lo e commiiteo

--- app/src/main/java/com/example/pracgrancentre/MainActivity.java
+++ app/src/main/java/com/example/pracgrancentre/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.pracgrancentre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView Restaurants = findViewById(R.id.restaurants);
+        ImageView movies = findViewById(R.id.movies);
+
+        Restaurants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getApplicationContext(), Restaurants.class);
+                startActivity(in);
+            }
+        });
+
+        movies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in2 = new Intent(getApplicationContext(), Movies.class);
+                startActivity(in2);
+            }
+        });
+
     }
 }
