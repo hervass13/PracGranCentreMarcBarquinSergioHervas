2bf0ddeb186fc17d3e76470da6d52cc9811966e7
Sergio Hervas <sergio@gmail.com>
2021-11-17 10:54:02 +0100

    inicialjh

--- app/src/main/java/com/example/pracgrancentre/MainActivity.java
+++ app/src/main/java/com/example/pracgrancentre/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.pracgrancentre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView Business;
+    ImageView Transport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Business = findViewById(R.id.business);
+
+        Business.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getApplicationContext(), business.class);
+                startActivity(in);
+            }
+        });
+
+        Transport = findViewById(R.id.transport);
+
+        Transport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getApplicationContext(), transport.class);
+                startActivity(in);
+            }
+        });
     }
 }
