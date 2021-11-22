aae896b04f8de064c1174beaf2d4409527b008fe
Sergio Hervas <sergio@gmail.com>
2021-11-22 18:46:49 +0100

    primer avance

--- app/src/main/java/com/example/pracgrancentre/transport.java
+++ app/src/main/java/com/example/pracgrancentre/transport.java
@@ -1,6 +1,7 @@
 package com.example.pracgrancentre;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class transport extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transport);
+
+        FragmentManager fm = getSupportFragmentManager();
+        if (fm.findFragmentById(R.id.contenidorTransp) == null) {
+            transportF TransportF = new transportF();
+            fm.beginTransaction().add(R.id.contenidorTransp, TransportF).commit();
+        }
     }
 }
--- app/src/main/java/com/example/pracgrancentre/transportF.java
+++ app/src/main/java/com/example/pracgrancentre/transportF.java
@@ -1,9 +1,7 @@
 package com.example.pracgrancentre;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +10,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  * Use the {@link transportF#newInstance} factory method to
  * create an instance of this fragment.
+ *
  */
 public class transportF extends Fragment {
 
@@ -23,10 +22,6 @@ public class transportF extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    public transportF() {
-        // Required empty public constructor
-    }
 
     /**
      * Use this factory method to create a new instance of
@@ -44,6 +39,10 @@ public class transportF extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public transportF() {
+        // Required empty public constructor
     }
 
     @Override
